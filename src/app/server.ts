58de47b29fdc4db0cdeba51c22c5e366955be80b
gustavoghp87 <ghp.2120@gmail.com>
2020-12-26 17:59:36 -0300

    server heroku

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,3 +1,5 @@
 //export const server = 'http://localhost:3000'
 //export const server = 'http://192.168.0.87:3000'
-export const server = 'https://181.229.192.23:3000'
+export const server = 'https://piso-de-mercado-restful.herokuapp.com'
+
+console.log(server)
