f5c35f47eec741062e9f757e61833bfb08bf0d4f
gustavoghp87 <ghp.2120@gmail.com>
2020-12-26 13:58:52 -0300

    https

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,3 +1,3 @@
 //export const server = 'http://localhost:3000'
 //export const server = 'http://192.168.0.87:3000'
-export const server = 'http://181.229.192.23:3000'
+export const server = 'https://181.229.192.23:3000'
