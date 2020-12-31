088b615734af1f37b041ae06fb3c5e0d14646640
gustavoghp87 <ghp.2120@gmail.com>
2020-12-31 16:21:50 -0300

    server

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,5 +1,10 @@
-export const server = 'http://localhost:3000'
-//export const server = 'http://192.168.0.87:3000'
-//export const server = 'https://piso-de-mercado-restful.herokuapp.com'
+export let server:string
+
+
+const local = false
+
+
+if (local) server = 'http://localhost:3000'
+else server = 'https://piso-de-mercado-restful.herokuapp.com'
 
 console.log(server)
